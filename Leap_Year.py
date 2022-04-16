4ebc1735f71bd35673fa416e1317417f8577de28
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-04-16 10:22:36 -0700

    added source file

---
+++ Leap_Year.py
@@ -0,0 +1,19 @@
+# a Leap year is divisible by 4 but not by 100 unless it is also divible by 400
+
+print("Leap year program\n")
+def main():
+    print("Insert leap year: ")
+    x = input()
+    print("You insereted " + x)
+
+    div = x % 4
+
+    if (div == 0) and (
+        continue:
+    if not div:
+        and 100:
+            and 400:
+        print("Is Leap Year\n")
+    else:
+        print("Not Leap Year\n")
+main()
