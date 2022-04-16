09e06582829ceb6ae5fb7371a7b70f61593b74f1
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-04-16 15:54:51 -0700

    added final code to souce file

--- Leap_Year.py
+++ Leap_Year.py
@@ -3,17 +3,13 @@
 print("Leap year program\n")
 def main():
     print("Insert leap year: ")
-    x = input()
-    print("You insereted " + x)
+    x = int(input())
+    print("You insereted ", + x)
 
     div = x % 4
 
-    if (div == 0) and (
-        continue:
-    if not div:
-        and 100:
-            and 400:
-        print("Is Leap Year\n")
+    if (div == 0) and (x / 100) and (x / 400):
+        print("is zero, is Leap Year\n")
     else:
         print("Not Leap Year\n")
 main()
